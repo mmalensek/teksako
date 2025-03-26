25cbcfaf951a0380a770c08102404177270fbd60
mmalensek <marto3304@gmail.com>
2025-03-26 21:02:18 +0100

    verzija 1.0.1 teksako.c

--- teksako.c
+++ teksako.c
@@ -1,7 +1,44 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode(){
+    // UGASNEMO ECHO in
+    // OMOGOČIMO RAW INPUT
+    // torej ni potrebno
+    // pritiskati ENTER
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    // ICANON ugasne cannonical mode
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main(){
+    enableRawMode();
+
     char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        // PREVERI ali je znak, kontrolni znak
+        // tega nočemo printat
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+
+    disableRawMode();
     return 0;
 }
